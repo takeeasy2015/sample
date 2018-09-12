79408741e1de508ae628c451bec98ab4691e6892
takeeasy2015 <takeeasy2015@gmail.com>
2018-09-13 07:01:49 +0800

    update: 使用通用視圖

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="zh-Hant-TW">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Sample App 主頁</title>
-</head>
-<body>
+@extends('layout.default')
+@section('content')
     <h1>主頁</h1>
-</body>
-</html>
+@stop
