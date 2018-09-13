e1df19e29ace9432835813fe545797d13f84daaa
takeeasy2015 <takeeasy2015@gmail.com>
2018-09-13 23:27:05 +0800

    update: 優化頁面內容.

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -5,8 +5,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Sample') - Laravel 新手入門</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    @yield('content')
+    <header class="navbar navbar-fixed-top navbar-inverse">
+        <div class="container">
+            <div class="col-md-10 col-md-offset-1">
+                <a href="/" id="logo">Sample App</a>
+                <nav>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="/help">幫助</a></li>
+                        <li><a href="#">登入</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    <div class="container">
+        @yield('content')
+    </div>
+    
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,10 @@
 @extends('layout.default')
 
 @section('content')
-    <h1>主頁</h1>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">您現在看到的是主頁</p>
+        <p>一切從這開始</p>
+        <a href="#" class="btn btn-lg btn-success" role="botton">註冊</a>
+    </div>
 @stop
