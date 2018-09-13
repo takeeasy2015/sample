e75093d3d336d3f1419c09ce9cf4b879e5c9251e
vagrant <vagrant@homestead>
2018-09-13 13:38:16 +0000

    Finish static pages.

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 class StaticPagesController extends Controller
 {
+
     public function home() {
         return view('static_pages/home');
     }
@@ -17,4 +18,5 @@ class StaticPagesController extends Controller
     public function about() {
         return view('static_pages/about');
     }
+    
 }
