7a9ee32c209c21f7188a2737761a74fe5116afc3
vagrant <vagrant@homestead>
2018-09-15 12:33:43 +0000

    Finish layout and routes

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -5,6 +5,6 @@
         <h1>Hello Laravel</h1>
         <p class="lead">您現在看到的是主頁</p>
         <p>一切從這開始</p>
-        <a href="#" class="btn btn-lg btn-success" role="botton">註冊</a>
+        <a href="{{ route('signup') }}" class="btn btn-lg btn-success" role="botton">註冊</a>
     </div>
 @stop
