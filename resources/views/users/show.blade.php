2b6ac25af4dd0864a66ab50a5e4051531f088dda
takeeasy2015 <takeeasy2015@gmail.com>
2018-09-22 11:36:50 +0800

    add: 新增顯示用戶頭像與名稱.

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,5 +2,11 @@
 @section('title', $user->name)
 
 @section('content')
-{{ $user->name }} - {{ $user->email }}
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <section class="user_info">
+            @include('shared._user_info', ['user' => $user])
+        </section>
+    </div>
+</div>
 @stop
