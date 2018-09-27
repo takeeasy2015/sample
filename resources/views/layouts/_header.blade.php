4dff09aa4489e5c53e232985bc9c301849792448
takeeasy2015 <takeeasy2015@gmail.com>
2018-09-27 21:47:16 +0800

    add: 修改佈局中的連結.

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -4,8 +4,31 @@
             <a href="{{ route('home') }}" id="logo">Sample App</a>
             <nav>
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="{{ route('help') }}">幫助</a></li>
-                    <li><a href="#">登入</a></li>
+                    @if (Auth::check())
+                        <li><a href="#">用戶列表</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                {{ Auth::user()->name }}  <b class="caret"></b>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="{{ route('users.show', Auth::user()->id) }}">個人中心</a></li>
+                                <li><a href="#">編輯資料</a></li>
+                                <li class="divider"></li>
+                                <li>
+                                    <a href="#" id="logout">
+                                        <form action="{{ route('logout') }}" method="POST">
+                                            {{ csrf_field() }}
+                                            {{ method_field('DELETE') }}
+                                            <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+                                        </form>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                    @else
+                        <li><a href="{{ route('help') }}">幫助</a></li>
+                        <li><a href="{{ route('login') }}">登入</a></li>
+                    @endif
                 </ul>
             </nav>
         </div>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -15,5 +15,7 @@
         @yield('content')
         @include('layouts._footer')
     </div>
+
+    <script src="/js/app.js"></script>
 </body>
 </html>
